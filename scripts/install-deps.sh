6eb5d200639b9fcba05d36c10c8578fa8b61f857
Ioannis Sermetziadis <ioannis@codebgp.com>
2021-05-20 01:14:25 +0300

    install go deps fix

---
+++ scripts/install-deps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+tmp_dir=$(mktemp -d)
+pushd $tmp_dir
+go get -u github.com/sqs/goreturns \
+	&& go get -u github.com/securego/gosec/v2/cmd/gosec@v2.7.0 \
+	&& go get -u golang.org/x/lint/golint \
+        && go get -u github.com/golangci/golangci-lint/cmd/golangci-lint@v1.40.0
+popd
+rm -rf $tmp_dir
+
+
